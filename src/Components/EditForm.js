56acd95600c84384fcfd04d38c99050639a9ef90
samliprajapati <samliprajapati9@gmail.com>
2020-11-29 13:38:33 +0530

    Remove un used items

--- src/Components/EditForm.js
+++ src/Components/EditForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Input, Button } from "antd";
+import { Button } from "antd";
 import { Formik, Form, Field } from "formik";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
